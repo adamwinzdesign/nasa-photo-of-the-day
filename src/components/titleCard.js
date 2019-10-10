02c08727ce93df0c541d587b5c9c61f68325e6dc
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-10 17:57:42 -0500

    added navbar

--- src/components/titleCard.js
+++ src/components/titleCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleCard = props => {
   return (
     <div className = 'title-card' key = '{props.id}'>
-      <h1>Title for today's Image: {props.title}</h1>
+      <h1>{props.title}</h1>
     </div>
   )
 }
