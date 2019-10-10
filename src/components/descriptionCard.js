48828443e8f18e6e2b8d9c83af9789c3371e6e42
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-10 16:34:06 -0500

    added reactstrap card to description

--- src/components/descriptionCard.js
+++ src/components/descriptionCard.js
@@ -1,12 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-const DescriptionCard = props => {
+// const DescriptionCard = props => {
+//   return (
+//     <div className = 'description-card' key = '{props.id}'>
+//       <h1>Description for image:</h1>
+//       <p>{props.explanation}</p>
+//     </div>
+//   )
+// }
+
+// export default DescriptionCard;
+
+import React from 'react';
+import {
+  Card, CardText, CardBody,
+  CardTitle
+} from 'reactstrap';
+
+const DescriptionCard = (props) => {
   return (
     <div className = 'description-card' key = '{props.id}'>
-      <h1>Description for image:</h1>
-      <p>{props.explanation}</p>
+      <Card body inverse color="success">
+        <CardBody>
+          <CardTitle><h1>Description for image:</h1></CardTitle>
+          <CardText>{props.explanation}</CardText>
+        </CardBody>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
 export default DescriptionCard;
